f04c5bebfde772a6f679dfca57937c9afcba8a09
John Vinh <john.michael.vinh2@gmail.com>
2022-09-30 21:13:50 -0400

    Added simple add functionality

--- BasicWPFCalculator/MainWindow.xaml.cs
+++ BasicWPFCalculator/MainWindow.xaml.cs
@@ -24,5 +24,33 @@ namespace BasicWPFCalculator
         {
             InitializeComponent();
         }
+
+        private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            // Make sure the first number is valid
+            try
+            {
+                num1 = int.Parse(TxtNum1.Text);
+            }
+            catch(System.FormatException)
+            {
+                MessageBox.Show("Please ensure number 1 is a valid number.");
+                return;
+            }
+            // Make sure the second number is valid
+            try
+            {
+                num2 = int.Parse(TxtNum2.Text);
+            }
+            catch(System.FormatException)
+            {
+                MessageBox.Show("Please ensure number 2 is a valid number.");
+                return;
+            }
+            // Both numbers are valid
+            LblResult.Content = num1 + num2;
+        }
     }
 }
